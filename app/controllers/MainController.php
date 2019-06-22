a5e9223533a5ea9ed16ed5519c488959dba4c825
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-06-22 20:06:56 +0300

    10

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,6 +10,12 @@ class MainController extends AppController
 
     public function indexAction() {
         //echo __METHOD__;
+        $this->setMeta('Главная страница', 'Описание.....', 'Ключевики');
+        $name = 'John';
+        $age = 30;
+        $names = ['Andrey', 'Jane'];
+        $this->set(compact('name', 'age', 'names'));
+
 
     }
 }
--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,1 +1,5 @@
 <h1>Hello world</h1>
+<p><?=$name;?></p>
+<p><?=$age;?></p>
+<?php debug($names);?>
+
